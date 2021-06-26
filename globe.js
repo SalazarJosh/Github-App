e4aa9cd6a9d6ed8d4796c57315b49064f696af5f
indijosh <indijosh@users.noreply.github.com>
2021-06-26 13:41:39 -0400

    globe improvements

--- globe.js
+++ globe.js
@@ -81,6 +81,133 @@ overlaySphere.castShadow = true;
 overlaySphere.receiveShadow = true;
 scene.add(overlaySphere);
 
+//set up bezier curves
+var numPoints = 100;
+//        var start = new THREE.Vector3(-5, 0, 20);
+var start = new THREE.Vector3(0, 1.5, 1.3);
+var middle = new THREE.Vector3(.6, .6, 3.2);
+var end = new THREE.Vector3(1.5, -1, .8);
+
+var curveQuad = new THREE.QuadraticBezierCurve3(start, middle, end);
+
+var tube1 = new THREE.TubeGeometry(curveQuad, numPoints, 0.01, 20, false);
+const tubeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xd965fa
+});
+tube1.setDrawRange(0, 10000);
+var curveMesh1 = new THREE.Mesh(tube1, tubeMaterial);
+scene.add(curveMesh1);
+
+var tube2 = new THREE.TubeGeometry(curveQuad, numPoints, 0.01, 20, false);
+tube2.setDrawRange(0, 10000);
+var curveMesh2 = new THREE.Mesh(tube2, tubeMaterial);
+sphere.add(curveMesh2);
+curveMesh2.rotation.y = .75
+curveMesh2.rotation.z = .75
+curveMesh2.rotation.x = -.1
+
+var tube3 = new THREE.TubeGeometry(curveQuad, numPoints, 0.01, 20, false);
+tube3.setDrawRange(0, 10000);
+var curveMesh3 = new THREE.Mesh(tube3, tubeMaterial);
+sphere.add(curveMesh3);
+curveMesh3.rotation.y = 2.1
+curveMesh3.rotation.z = .5
+curveMesh3.rotation.x = .2
+
+var tube4 = new THREE.TubeGeometry(curveQuad, numPoints, 0.01, 20, false);
+tube4.setDrawRange(0, 10000);
+var curveMesh4 = new THREE.Mesh(tube4, tubeMaterial);
+sphere.add(curveMesh4);
+curveMesh4.rotation.y = 2.3
+curveMesh4.rotation.z = .8
+curveMesh4.rotation.x = .2
+
+var tube5 = new THREE.TubeGeometry(curveQuad, numPoints, 0.01, 20, false);
+tube5.setDrawRange(0, 10000);
+var curveMesh5 = new THREE.Mesh(tube5, tubeMaterial);
+sphere.add(curveMesh5);
+curveMesh5.rotation.y = 2.9
+curveMesh5.rotation.z = 1.1
+curveMesh5.rotation.x = 2
+
+var tube6 = new THREE.TubeGeometry(curveQuad, numPoints, 0.01, 20, false);
+tube6.setDrawRange(0, 10000);
+var curveMesh6 = new THREE.Mesh(tube6, tubeMaterial);
+sphere.add(curveMesh6);
+curveMesh6.rotation.y = 7.1
+curveMesh6.rotation.z = 1
+curveMesh6.rotation.x = 4.4
+
+var tube7 = new THREE.TubeGeometry(curveQuad, numPoints, 0.01, 20, false);
+tube7.setDrawRange(0, 10000);
+var curveMesh7 = new THREE.Mesh(tube7, tubeMaterial);
+sphere.add(curveMesh7);
+curveMesh7.rotation.y = 2.1
+curveMesh7.rotation.z = 3
+curveMesh7.rotation.x = 4.4
+
+var tube8 = new THREE.TubeGeometry(curveQuad, numPoints, 0.01, 20, false);
+tube8.setDrawRange(0, 10000);
+var curveMesh8 = new THREE.Mesh(tube8, tubeMaterial);
+sphere.add(curveMesh8);
+curveMesh8.rotation.y = 2.5
+curveMesh8.rotation.z = 1
+curveMesh8.rotation.x = 1.1
+
+
+
+//set up spires
+const cylinderGeometry = new THREE.CylinderGeometry(.01, .01, 4.25, 32);
+const cylinderMaterial = new THREE.MeshBasicMaterial({
+  color: 0x00ddff,
+  transparent: true,
+  opacity: .5
+});
+const cylinderMaterial2 = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  transparent: true,
+  opacity: .5
+});
+const cylinder1 = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+sphere.add(cylinder1);
+cylinder1.rotation.x = .75;
+
+const cylinder7 = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+sphere.add(cylinder7);
+cylinder7.rotation.x = .74;
+cylinder7.rotation.z = -.05;
+
+const cylinder8 = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+sphere.add(cylinder8);
+cylinder8.rotation.x = .72;
+cylinder8.rotation.z = -.07;
+
+
+const cylinder2 = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+sphere.add(cylinder2);
+cylinder2.rotation.x = -1;
+cylinder2.rotation.z = 2;
+
+const cylinder3 = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+sphere.add(cylinder3);
+cylinder3.rotation.x = .8;
+cylinder3.rotation.z = .5;
+
+const cylinder4 = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+sphere.add(cylinder4);
+cylinder4.rotation.x = 1.05;
+cylinder4.rotation.z = 0;
+
+const cylinder5 = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+sphere.add(cylinder5);
+cylinder5.rotation.x = 2;
+cylinder5.rotation.z = 3;
+
+const cylinder6 = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+sphere.add(cylinder6);
+cylinder6.rotation.x = .8;
+cylinder6.rotation.z = 2.5;
+
 //Detect click-drag rotation
 /* */
 var isDragging = false;
@@ -116,20 +243,50 @@ $("#canvas").mouseout(function() {
   isDragging = false;
 });
 
-
 //SETUP camera
 const camera = new THREE.PerspectiveCamera(75, 900 / 900, 0.1, 1000);
 camera.position.z = 6;
 
+var renderCount = 0;
+var currentGrowing = 0;
+var tubes =[tube1,tube2,tube3,tube4,tube5,tube6,tube7,tube8]
+
+function GrowTube(index, renderCount){
+  tubes[index].setDrawRange(0, renderCount)
+  if(index > 2){
+    tubes[index - 3].setDrawRange(renderCount, 10000)
+  }
+  else{
+    tubes[(tubes.length - 3) + index].setDrawRange(renderCount, 10000)
+  }
+}
+
 //ANIMATION LOOP
 const animate = function() {
+  if (renderCount < 10000) {
+    renderCount += 80;
+    GrowTube(currentGrowing, renderCount);
+  }
+
+  else{
+    renderCount = 0;
+
+    if(currentGrowing >= tubes.length - 1){
+      currentGrowing = 0;
+    }
+    else{
+      currentGrowing++;
+    }
+  }
+
   requestAnimationFrame(animate);
 
   if (!isDragging) {
-    sphere.rotation.y += 0.001;
+    //sphere.rotation.y += 0.001;
   }
 
   overlaySphere.rotation.y = sphere.rotation.y;
+  curveMesh1.rotation.y = sphere.rotation.y;
 
   renderer.render(scene, camera);
   container.appendChild(renderer.domElement);
